e349738a65397ba3fbb646e6741ad0ceffc42311
ZubairARooghwall <99389773+ZubairARooghwall@users.noreply.github.com>
2023-02-16 13:53:41 +0430

    Create a function to evaluate the decision and declare the winner

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ function getComputerChoice(){
  }
 
 function getUserChoice(){
-    let input = parseInt(prompt("Enter a number: (1 => Rock, 2 => Paper, 3 => Scissors"));
+    let input = prompt("Enter a value: (rock, paper, scissors)");
     let returnValue;
-        if(input == 1) returnValue = "Rock";
-        else if(input == 2) returnValue = "Paper";
-        else if(input == 3) returnValue = "Scissors"
+        if(input === "Rock" || input === "rock" || input === "ROCK" || input === "RocK" || input === "rOCk") returnValue = "Rock";
+        else if(input === "Paper" || input === "paper" || input === "PAPER" || input === "PapeR") returnValue = "Paper";
+        else if(input === "Scissors" || input === "Scissor" || input === "scissors" || input == "scissor") returnValue = "Scissors"
 
     return returnValue;
 }
+
+function checkOutput(user, computer){
+    let returnValue;
+        if(user == computer) returnValue = "Draw: No one wins";
+        else if(user == "rock"  && computer == "paper") returnValue = "Computer won\n\tPaper beats rock";
+        else if(user == "paper" && computer == "rock") returnValue = "User won\n\tPaper beats rock";
+        else if(user == "rock" && computer == "scissors") returnValue = "User won\n\tRock beats scissors";
+        else if(user == "scissors" && computer == "rock") returnValue = "Computer won\n\tRock beats scissors";
+        else if(user == "paper" && computer == "scissors") returnValue = "Computer won\n\tScissors beat paper";
+        else if(user == "scissors" && "paper") returnValue = "User won\n\tScissors beat paper";
+
+    return returnValue;
+}
+
 
